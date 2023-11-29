d13dfd7b3c968a72d604e655ecf96ae2d1ce1657
Joel Oliver Millán <jolivermillan@gmail.com>
2023-11-29 14:45:51 +0100

    improved navbar styles and added icons and new logo

--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -1,14 +1,31 @@
 import styles from "../NavBar/styles.module.css";
 import SearchBar from "../SearchBar";
+import logo from "../../assets/icons/logo.svg";
+import userIcon from "../../assets/icons/user-svgrepo-com.svg";
+import listIcon from "../../assets/icons/list-ul-alt-svgrepo-com.svg";
+import locationIcon from "../../assets/icons/location-pin-svgrepo-com.svg";
 
 export default function NavBar() {
   return (
     <nav className={styles.navBar}>
-      <a href="">Glotón</a>
+      <a href="">
+        <img className={styles.logo} src={logo} alt="" />
+      </a>
       <SearchBar />
-      <p>Adress, 98, 3016. Barcelona</p>
-      <button>P</button>
-      <button>L</button>
+      <div className={styles.rightContainer}>
+        <div className={styles.locationContainer}>
+          <img className={styles.locationIcon} src={locationIcon} alt="" />
+          <p>Adress, 98, 3016. Barcelona</p>
+        </div>
+        <div className={styles.navBarButtons}>
+          <button>
+            <img className={styles.userIcon} src={userIcon} alt="" />
+          </button>
+          <button>
+            <img className={styles.listIcon} src={listIcon} alt="" />
+          </button>
+        </div>
+      </div>
     </nav>
   );
 }
--- frontend/src/components/SearchBar/index.jsx
+++ frontend/src/components/SearchBar/index.jsx
@@ -1,9 +1,17 @@
 import styles from "../SearchBar/styles.module.css";
+import searchIcon from "../../assets/icons/search-svgrepo-com.svg";
 
 export default function SearchBar() {
   return (
     <div className={styles.searchBar}>
-      <input type="text" placeholder="Search" />
+      <div className={styles.inputContainer}>
+        <img className={styles.searchIcon} src={searchIcon} alt="" />
+        <input
+          className={styles.searchInput}
+          type="text"
+          placeholder="Search"
+        />
+      </div>
     </div>
   );
 }
