a80cb2bf2870278c63e98b7af3571572a4c89690
Joel Oliver Millán <jolivermillan@gmail.com>
2023-12-01 13:59:42 +0100

    Hero page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import "../src/index.css";
 import React from "react";
+import HeroPage from "./components/HeroPage";
 
 function App() {
-  return <div>Your App here</div>;
+  return (
+    <div>
+      <HeroPage />
+      {/* Your App here */}
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -5,31 +5,48 @@ import userIcon from "../../assets/icons/user-svgrepo-com.svg";
 import listIcon from "../../assets/icons/list-ul-alt-svgrepo-com.svg";
 import locationIcon from "../../assets/icons/location-pin-svgrepo-com.svg";
 
+const logged = true;
+
 export default function NavBar() {
-  return (
-    <nav className={styles.navBar}>
-      <div className={styles.logoContainer}>
-        <a href="">
-          <img className={styles.logo} src={logo} alt="" />
-        </a>
-      </div>
-      <div className={styles.searchBarContainer}>
-        <SearchBar />
-      </div>
-      <div className={styles.rightContainer}>
-        <div className={styles.locationContainer}>
-          <img className={styles.locationIcon} src={locationIcon} alt="" />
-          <p>Adress, 98, 3016. Barcelona</p>
-        </div>
-        <div className={styles.navBarButtons}>
-          <button>
-            <img className={styles.userIcon} src={userIcon} alt="" />
-          </button>
-          <button>
-            <img className={styles.listIcon} src={listIcon} alt="" />
-          </button>
-        </div>
-      </div>
-    </nav>
-  );
+  {
+    if (logged === true)
+      return (
+        <nav className={styles.navBar}>
+          <div className={styles.logoContainer}>
+            <a href="">
+              <img className={styles.logo} src={logo} alt="" />
+            </a>
+          </div>
+          <div className={styles.searchBarContainer}>
+            <SearchBar />
+          </div>
+          <div className={styles.rightContainer}>
+            <div className={styles.locationContainer}>
+              <img className={styles.locationIcon} src={locationIcon} alt="" />
+              <p>Adress, 98, 3016. Barcelona</p>
+            </div>
+            <div className={styles.navBarButtons}>
+              <button>
+                <img className={styles.userIcon} src={userIcon} alt="" />
+              </button>
+              <button>
+                <img className={styles.listIcon} src={listIcon} alt="" />
+              </button>
+            </div>
+          </div>
+        </nav>
+      );
+    else {
+      return (
+        <nav className={styles.navBar}>
+          <div className={styles.logoContainer}>
+            <a href="">
+              <img className={styles.logo} src={logo} alt="" />
+            </a>
+          </div>
+          <button className={styles.getStartedButton}>Empieza aquí</button>
+        </nav>
+      );
+    }
+  }
 }
