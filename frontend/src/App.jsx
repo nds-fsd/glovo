89e8ab7ebd3f3b1218edfd115a4e72867cfffd7d
Joel Oliver Millán <jolivermillan@gmail.com>
2023-12-19 15:39:19 +0100

    Creado el fetching de restaurantes y productos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,16 @@
-
 import "../src/index.css";
 import RestaurantPage from "./components/RestaurantPage";
 import HomePage from "./components/HomePage/index";
 import NavBar from "./components/NavBar/index";
 import HeroPage from "./components/HeroPage/index";
 import PerfilUsuario from "./components/PerfilUsuario/PerfilUsuario";
-import React from "react";
 
 function App() {
   return (
     <>
-      <NavBar />;
-      <PerfilUsuario />;
-      <HeroPage />;
-      <HomePage />;
-     <RestaurantPage />
+      <RestaurantPage />;
     </>
-
   );
-
 }
 
-export default App
+export default App;
