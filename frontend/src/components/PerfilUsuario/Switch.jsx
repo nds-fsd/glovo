0d4a821644f91e75adcf7781aa7778f3fa935cb8
Jose Garcia <josegarcia1006@gmail.com>
2023-12-19 23:09:55 +0100

    separacion switch

---
+++ frontend/src/components/PerfilUsuario/Switch.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Switch = ({ isOn, handleToggle }) => {
+  return (
+    <>
+      <input
+        className="react-switch-checkbox"
+        id={`react-switch-new`}
+        type="checkbox"
+        checked={isOn}
+        onChange={handleToggle}
+      />
+      <label
+        className="react-switch-label"
+        htmlFor={`react-switch-new`}
+        style={{ background: isOn && "#06D6A0" }}
+      >
+        <span className={`react-switch-button`} />
+      </label>
+    </>
+  );
+};
+
+export default Switch;
