a07b2ac27eeb083b2f0e4019798439ee249162ef
Joel Oliver Millán <jolivermillan@gmail.com>
2024-01-11 19:25:49 +0100

    First commit

--- backend/src/schema/usersSchema.js
+++ backend/src/schema/usersSchema.js
@@ -1,14 +1,29 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-    firstname: String,
-    lastname: String,
-    encrypted_password: String,
-    created_date: { type: Date, default: Date.now },
-    email: String,
-    phone: String
+  firstname: String,
+  lastname: String,
+  password: String,
+  created_date: { type: Date, default: Date.now },
+  email: String,
+  phone: String,
+});
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
   });
+});
 
-  const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
-  module.exports = User;
+module.exports = User;
