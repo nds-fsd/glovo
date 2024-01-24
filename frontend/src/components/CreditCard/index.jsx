41a57f6b70c96961f1342f3d9de7d43263a9b965
Joel Oliver <jolivermillan@gmail.com>
2024-01-24 14:44:14 +0100

    added animation

--- frontend/src/components/CreditCard/index.jsx
+++ frontend/src/components/CreditCard/index.jsx
@@ -3,17 +3,26 @@ import styles from "../CreditCard/styles.module.css";
 import Modal from "react-modal";
 import flagIcon from "../../assets/icons/flag-svgrepo-com.svg";
 import walletIcon from "../../assets/icons/credit-card-svgrepo-com.svg";
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router";
 
 Modal.setAppElement("#root");
 
 export default function CreditCard({ modalIsOpen, shoppingList, productos }) {
+  const navigate = useNavigate();
+
   return (
     <Modal
       className={styles.modalContent}
       isOpen={modalIsOpen}
       overlayClassName={styles.modalOverlay}
     >
-      <div className={styles.mainContainer}>
+      <motion.div
+        initial={{ opacity: 0, translateY: 50 }}
+        animate={{ opacity: 1, translateY: 0 }}
+        transition={{ ease: "easeOut", duration: 0.2 }}
+        className={styles.mainContainer}
+      >
         <h2>Buen provecho!</h2>
         <div className={styles.itemContainer}>
           <p>Resumen de tu pedido</p>
@@ -48,8 +57,13 @@ export default function CreditCard({ modalIsOpen, shoppingList, productos }) {
             <b className={styles.modifiableItem}>**** **** **** 4278</b>
           </p>
         </div>
-        <button className={styles.confirmButton}>Confirmar pedido</button>
-      </div>
+        <button
+          onClick={() => navigate("/confirmation")}
+          className={styles.confirmButton}
+        >
+          Confirmar pedido
+        </button>
+      </motion.div>
     </Modal>
   );
 }
