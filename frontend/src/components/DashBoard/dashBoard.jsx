dc4b27278867d3e8246a1d49f90d731fb1cfe623
zurab <metreveli.zura.2014@gmail.com>
2024-01-28 17:12:55 +0100

    Ultimos cambios de DashBoard

--- frontend/src/components/DashBoard/dashBoard.jsx
+++ frontend/src/components/DashBoard/dashBoard.jsx
@@ -1,17 +1,29 @@
 import React, { useEffect, useState } from "react";
 import styles from "./styles.module.css";
-import axios from "axios";
-import foodImg from "../../assets/images/burger.jpg";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const DashBoard = () => {
-  const [products, setProducts] = useState([]);
+  const [setProducts] = useState([]);
   const [restaurantProducts, setRestaurantProducts] = useState([]);
+  const [newProduct, setNewProduct] = useState("");
+
   const [searchTerm, setSearchTerm] = useState(""); // Nuevo estado para el término de búsqueda
   const [restaurantId, setrestaurantId] = useState([]);
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
+  const products = [
+    {
+      id: 1,
+      name: "Producto 1",
+      image: "../../assets/images/producteexampleimg.avif",
+    },
+    { id: 2, name: "Producto 2", image: "ruta/a/imagen2.jpg" },
+    // Más productos...
+  ];
 
   // Filtrar productos basados en el término de búsqueda
   const filteredProducts = products.filter((product) =>
@@ -21,6 +33,14 @@ const DashBoard = () => {
   const filteredRestaurantProducts = restaurantProducts.filter((product) =>
     product.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // Otros ajustes según necesites
+  };
 
   return (
     <div className={styles.container}>
@@ -34,11 +54,15 @@ const DashBoard = () => {
             ))}
           </ul>
         ) : (
-          <p>No restaurant inf. found.</p>
+          <p></p>
         )}
       </div>
 
       <div className={styles.divFondoPantalla}>
+        <div className={styles.buttons}>
+          <button className={styles.switch}>Switch</button>
+          <button className={styles.close}>Close</button>
+        </div>
         <input
           className={styles.input}
           type="text"
@@ -53,8 +77,21 @@ const DashBoard = () => {
             ))}
           </ul>
         ) : (
-          <p className={styles.p}>No restaurant-specific products found.</p>
+          <p className={styles.p}></p>
         )}
+        <Slider className={styles.slider} {...settings}>
+          {products.map((product) => (
+            <div key={product.id}>
+              <img src={product.image} alt={product.name} />
+              <p>{product.name}</p>
+              {/* Otros detalles del producto */}
+            </div>
+          ))}
+        </Slider>
+        <div className={styles.buttons2}>
+          <button className={styles.add}>Añadir</button>
+          <button className={styles.delete}>Borrar</button>
+        </div>
       </div>
     </div>
   );
