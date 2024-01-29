8850ed01cd031af0a26845faa7fa932f8007c8ba
Joel Oliver Millán <jolivermillan@gmail.com>
2024-01-29 19:58:03 +0100

    Fixed .env variable

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,16 +1,18 @@
-import axios from 'axios';
-import { getUserToken } from '../utils/localStorage.utils';
+import axios from "axios";
+import { getUserToken } from "../utils/localStorage.utils";
+
+const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3001',
-    headers: {
-      'Content-Type': 'application/json',
-    }
+  baseURL: `${BASE_URL}`,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-export const  objectToQueryString = (obj) => {
-    const queryString = Object.keys(obj)
-      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`)
-      .join('&');
-    return queryString;
-}
+export const objectToQueryString = (obj) => {
+  const queryString = Object.keys(obj)
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`)
+    .join("&");
+  return queryString;
+};
