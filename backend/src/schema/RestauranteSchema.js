b58577cffd5a75819ecf6959a87464893f206255
zurab <metreveli.zura.2014@gmail.com>
2024-01-31 17:55:50 +0100

    commit rama 55

--- backend/src/schema/RestauranteSchema.js
+++ backend/src/schema/RestauranteSchema.js
@@ -3,16 +3,16 @@ const mongoose = require("mongoose");
 const restauranteSchema = new mongoose.Schema({
   email: String,
   password: String,
-  nombre: String,
-  descripcion: String,
-  direccion: String,
-  categoria: String,
-  horarios: String,
-  imagen: String,
-  transporte: String,
-  oferta: String,
-  puntuacion: String,
-  votos: String,
+  country: String,
+  city: String,
+  brandName: String,
+  firstName: String,
+  lastName: String,
+  phone: String,
+  category: String,
+  whatsapp: Boolean,
+  privacy: Boolean,
+  incluirCodigo: Boolean,
 });
 
 const Restaurante = mongoose.model("Restaurante", restauranteSchema);
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,30 +1,34 @@
-import axios from 'axios';
-import { setStorageObject, setUserSession, getUserToken } from '../utils/localStorage.utils'; 
+import axios from "axios";
+import {
+  setStorageObject,
+  setUserSession,
+  getUserToken,
+} from "../utils/localStorage.utils";
 
 // Configuración de Axios
 export const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: "http://localhost:3001",
   headers: {
-    'Content-Type': 'application/json',
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 api.interceptors.request.use((config) => {
   const token = getUserToken();
-  config.headers.Authorization = token ? `Bearer ${token}` : '';
+  config.headers.Authorization = token ? `Bearer ${token}` : "";
   return config;
 });
 
 // Función para iniciar sesión
 export const login = async (credentials) => {
   try {
-    const response = await api.post('/login', credentials);
-    console.log(response)
+    const response = await api.post("/login", credentials);
+    console.log(response);
     // setStorageObject("user-session", response.data)
     setUserSession({ token: response.data.token, user: response.data.user });
     return response.data;
   } catch (error) {
-    console.error('Error durante el inicio de sesión:', error);
+    console.error("Error durante el inicio de sesión:", error);
     throw error;
   }
 };
@@ -32,20 +36,32 @@ export const login = async (credentials) => {
 // Función para registrar un nuevo usuario
 export const register = async (userData) => {
   try {
-    const response = await api.post('/register', userData);
-    setStorageObject("token", response.data.token)
-    setStorageObject("user", response.data.user)
+    const response = await api.post("/register", userData);
+    setStorageObject("token", response.data.token);
+    setStorageObject("user", response.data.user);
     // setUserSession({ token: response.data.token, user: response.data.user });
     return response.data;
   } catch (error) {
-    console.error('Error durante el registro:', error);
+    console.error("Error durante el registro:", error);
     throw error;
   }
 };
 
 export const objectToQueryString = (obj) => {
   const queryString = Object.keys(obj)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`)
-    .join('&');
+    .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`)
+    .join("&");
   return queryString;
+};
+
+// *------------------------Crear Restaurante--------------------------
+
+export const createRestaurant = async (restaurantData) => {
+  try {
+    const response = await api.post("/restaurantes", restaurantData);
+    return response.data;
+  } catch (error) {
+    console.error(error.response ? error.response.data : error.message);
+    throw error;
+  }
 };
