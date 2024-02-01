ce2efeabda351235a125d6b9e1b89a4c2df2d76c
zurab <metreveli.zura.2014@gmail.com>
2024-02-01 19:38:59 +0100

    commit de img modificados

--- backend/src/schema/RestauranteSchema.js
+++ backend/src/schema/RestauranteSchema.js
@@ -13,6 +13,11 @@ const restauranteSchema = new mongoose.Schema({
   whatsapp: Boolean,
   privacy: Boolean,
   incluirCodigo: Boolean,
+  img: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1551782450-17144efb9c50?q=80&w=2969&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
 });
 
 const Restaurante = mongoose.model("Restaurante", restauranteSchema);
