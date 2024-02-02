133eed92c166e75550f83bf323497538f4ee9018
Joel Oliver Millán <jolivermillan@gmail.com>
2024-02-02 14:00:19 +0100

    minor change to address modal now on submit empties everything

--- frontend/src/components/AddressModal/index.jsx
+++ frontend/src/components/AddressModal/index.jsx
@@ -21,6 +21,7 @@ export default function AddressModal({
     handleSaveClickAddress(formattedAddress);
     changeModalState();
     closeAddressModal();
+    setCoordinates("");
   };
 
   return (
