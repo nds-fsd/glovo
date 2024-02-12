629e88cf8cb2d14149214396142e12d1482bb479
Vincent <Vincent.rey865@icloud.com>
2024-02-12 22:32:28 +0100

    cypress config Login

--- e2e/cypress/e2e/auth/login.cy.js
+++ e2e/cypress/e2e/auth/login.cy.js
@@ -3,7 +3,23 @@ describe("Login Flow", () => {
     cy.visit("http://localhost:3003");
   });
 
-  it("can click empieza aqui button", () => {
-    cy.contains("Empieza aquí");
+  it("Login", () => {
+    cy.contains("Empieza aquí").click();
+    cy.contains("Accede").click();
+    cy.get('input[placeholder="Email"]').first().type("test@cypress.com");
+    cy.get('input[placeholder="Contraseña"]').type("Cypress123");
+    cy.contains("Iniciar Sesión").click();
+    cy.get('input[placeholder="Cuál es tu dirección?"]').type("nuclio");
+    cy.get("ul > li").first().click();
+    cy.get("._restaurantGrid_1be8d_15", { timeout: 10000 })
+      .should("be.visible")
+      .find("._mainContainer_17otr_1")
+      .first()
+      .click();
+    // cy.get('button[class="button._addToCart_1e8zj_50"]').first().click();
   });
 });
+
+// it("Hacer un pedido", () => {
+//   cy.get('input[placeholder="Cuál es tu dirección?"]').type("nuclio");
+// });
